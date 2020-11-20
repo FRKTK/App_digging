b769b23d27f28c456de4b21dc2d9b06e82239e78
andurand.thomas <andurand.thomas@gmail.com>
2020-11-20 13:34:07 +0100

    création du formulaire

---
+++ symFront/components/form/registerForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <b-form>
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+        description="We'll never share your email with anyone else."
+      >
+        <b-form-input
+          id="input-1"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-2" label="Your Name:" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          required
+          placeholder="Enter name"
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group id="input-group-3" label="Food:" label-for="input-3">
+        <b-form-select id="input-3" required></b-form-select>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+import { BForm } from "bootstrap-vue";
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
